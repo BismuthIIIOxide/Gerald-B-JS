cbda5089c30ef0f0493e300767f1f5519909a3c7
BismuthIIIOxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2021-11-16 15:10:07 -0500

    change everything

--- index.js
+++ index.js
@@ -7,24 +7,11 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log("################################\nSUCK MY NUTS!\n################################");
 
-    const BF = 'command-base.js'
-    const commandBase = require(`./commands/${BF}`)
-    const readCommands = dir => {
-        const files = fs.readdirSync(path.join(__dirname, dir))
-        for(const file of files){
-            const stat = fs.lstatSync(path.join(__dirname, dir, file))
-
-            if(stat.isDirectory()){
-                readCommands(path.join(dir, file))
-            } else if (file !== BF) {
-                const option = require(path.join(__dirname, dir, file))
-                commandBase(client, option)
-            }
-        }
-    }
-    readCommands("commands")
-
 })
+
+client.on("message", async (message) => {
+    require("./events/message")(client, message);
+});
 
 client.login(process.env.TOKEN)
 
