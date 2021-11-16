4cd5c5f2aded24f0260f465cc53d55fcbe5204a7
BismuthIIIOxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2021-11-16 15:27:02 -0500

    fix #1

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@ const {prefix} = require('../config.json')
 
 module.exports = async (bot, message) => {
     message.channel.messages.fetch();
-    if (message.channel.id === "702972566419144875") bot.channels.get("427546996178419714").send(message)
+    if (message.channel.id === "702972566419144875") bot.channels.cache.get("427546996178419714").send(message)
 
     if (message.author.bot) return;
     if (message.content.toLowerCase().startsWith(prefix)) return;
