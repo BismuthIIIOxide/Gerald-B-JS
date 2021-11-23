77db8ead5549cdcfe32cf74827e9d6e50252ace8
BismuthIIIOxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2021-11-23 15:12:40 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ module.exports = async (bot, message) => {
     //message.channel.messages.fetch();
     if (message.channel.id === "702972566419144875" || message.channel.id === "907826744235532308" || message.channel.id === "902176155954712616" || message.channel.id === "851855270685835264" ){
         bot.channels.get("910265854510366741").send(`from ${message.author.username} in ${message.channel.name}:\n${message}`)
-        // console.log(message)
+        console.log(message)
     }
 
 }
