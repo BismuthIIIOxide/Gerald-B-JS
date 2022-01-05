0be9e62281bb85d0cd443325403695950523bc62
BismuthIIIOxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2022-01-05 16:21:42 -0500

    Update kill urself.js

--- commands/cmd/kill urself.js
+++ commands/cmd/kill urself.js
@@ -1,8 +1,8 @@
 module.exports = {
     commands: 'kms', 
     expectedArgs: '',
-    minArgs: 1,
-    maxArgs: 1, 
+    minArgs: 0,
+    maxArgs: 0, 
     callback: (message, arguments, text) =>{
         message.channel.send("https://www.youtube.com/watch?v=ACCKkg9sH5M")
     }
