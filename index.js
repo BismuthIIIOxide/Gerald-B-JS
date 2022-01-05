430beef2648d05c57d672bd7cd2794203478cae7
BismuthIIIOxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2022-01-05 16:14:37 -0500

    commands test

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const path = require('path');
 const fs = require('fs');
 
 const client = new Discord.Client();
-// const config = require('./config.json');
+const config = require('./config.json');
 client.on('ready', () => {
     console.log("################################\nSUCK MY NUTS!\n################################");
+    const BF = 'command-base.js'
+    const commandBase = require(`./commands/${BF}`)
+    const readCommands = dir => {
+        const files = fs.readdirSync(path.join(__dirname, dir))
+        for(const file of files){
+            const stat = fs.lstatSync(path.join(__dirname, dir, file))
 
+            if(stat.isDirectory()){
+                readCommands(path.join(dir, file))
+            } else if (file !== BF) {
+                const option = require(path.join(__dirname, dir, file))
+                commandBase(client, option)
+            }
+        }
+    }
+    readCommands("commands")
 })
 
 client.on("message", async (message) => {
