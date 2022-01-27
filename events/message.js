b0addc66490af4ad1714e833de8c9bd26a51cae9
Bismuth(III) Oxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2022-01-26 22:05:06 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,7 +3,8 @@ const {prefix} = require('../config.json')
 module.exports = async (bot, message) => {
     //message.channel.messages.fetch();
     if (message.channel.id === "851855270685835264" || message.channel.id === "702972566419144875"){
-        console.log(message)
+        console.log(message.embeds[0])
+	 console.log((message.embeds).length)
         bot.channels.get("914703147014963230").send(`from ${message.author.username} in ${message.channel.name}:\n${message}`)
         if((message.embeds).length == 1){
             bot.channels.get("914703147014963230").send({embed: message.embeds[0]})
