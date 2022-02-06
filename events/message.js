1e5b8f37678613ae1f76dfffdfa23f53f074e47e
Bismuth(III) Oxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2022-02-06 01:09:50 +0000

    s

--- events/message.js
+++ events/message.js
@@ -6,16 +6,17 @@ module.exports = async (bot, message) => {
     if (message.channel.id === "851855270685835264" || message.channel.id === "702972566419144875"){
         bot.channels.get("914703147014963230").send(`from ${message.author.username} in ${message.channel.name}:\n${message}`)
         if((message.embeds).length == 1){
-		const msg = message.embeds[0]
-		const embed1 = new MessageEmbed()
-			.setColor("#0099ff")
-			.setTitle(msg.title)
-			.setURL(msg.url)
-			.setDescription(msg.description)
-		console.log(embed1)
-            bot.channels.get("914703147014963230").send({embed: [embed1]})
+            const msg = message.embeds[0]
+            const embed1 = new MessageEmbed()
+                .setColor("#0099ff")
+                .setTitle(msg.title)
+                .setURL(msg.url)
+                .setDescription(msg.description)
+            console.log(embed1)
+                bot.channels.get("914703147014963230").send({embed: [embed1]})
         }
-    } else {
+        console.log(message)
+    } else if(false == true) {
         var msg = message
         if (msg.toLowerCase().startsWith('g!status ')){
             const arguments = content.split(/[ ]+/)
